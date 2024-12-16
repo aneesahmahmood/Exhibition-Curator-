6a9e416d741055e0d23adca990a5d00eddffd987
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-12-16 21:15:59 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Welcome to ArtExplorer
+      </h1>
+    </div>
+  );
+}
+
+export default App;
